409a7f5bc11672ac57807a47a5bc389d3774057f
Alexey <flash-kir@yandex.ru>
2022-09-14 15:02:46 +0300

    download 10 books from tululu.org

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import requests
 
-def main():
-    url = 'https://tululu.org/txt.php?id=32168'
+def download_file_by_id(id, dir):
+    url = f'https://tululu.org/txt.php?id={id}'
     response = requests.get(url)
     response.raise_for_status()
-    book_file_name = '«Пески Марса» Артура Кларка.txt'
-
-    with open(book_file_name, 'wb') as file:
+    file_name = f'{dir}/book_{id}'
+    with open(file_name, 'wb') as file:
         file.write(response.content)
+
+
+
+def main():
+    for i in range(10):
+        download_file_by_id(i+1, 'books')
 
 if __name__ == '__main__':
     main()
