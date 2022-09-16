1292aaa32f2f9fe453f94481528a83ad08a391f8
Alexey <flash-kir@yandex.ru>
2022-09-16 15:31:31 +0300

    download book covers

--- main.py
+++ main.py
@@ -4,23 +4,32 @@ from urllib.error import HTTPError
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filepath
+from urllib.parse import urlparse
 
 
-def get_book_title(id):
+def get_book_data(id):
     url = f'https://tululu.org/b{id}/'
     response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
+    check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
     title_block = soup.find('body').find('div', id='content').find('h1')
     title, name = title_block.text.split('::')
-    return sanitize_filepath(f'{id}. {title.strip()} - {name.strip()}.txt')
+
+    image_path = soup.find('div', class_='bookimage').find('img')['src']
+
+    return {
+        'filename': sanitize_filepath(f'{id}. {title.strip()} - {name.strip()}.txt'),
+        'image_path': f'https://tululu.org/{image_path}'
+    }
+    
 
 def check_for_redirect(response):
     if response.status_code // 100 == 3:
         raise HTTPError("Не найдена книга")
 
 
-def download_file_by_id(url, filename, folder='books/'):
+def download_file(url, filename, folder='books/'):
     response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
     check_for_redirect(response)
@@ -29,14 +38,25 @@ def download_file_by_id(url, filename, folder='books/'):
         file.write(response.content)
 
 
+def download_image(url, folder='images/'):
+    response = requests.get(url, allow_redirects=False)
+    response.raise_for_status()
+    check_for_redirect(response)
+    filename = url.split('/')[-1]
+    os.makedirs(f'{folder}', exist_ok=True)
+    with open(f'{folder}/{filename}', 'wb') as file:
+        file.write(response.content)
+
+
 def main():
-    for id in range(11):
+    for id in range(1, 10):
         try:
-            download_file_by_id(
+            book_data = get_book_data(id)
+            download_file(
                 f'https://tululu.org/txt.php?id={id}', 
-                get_book_title(id)
+                book_data['filename']
             )
-            
+            download_image(book_data['image_path'])
         except:
             HTTPError
 
