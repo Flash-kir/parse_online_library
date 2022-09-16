ce9836ddc6b41ed70d0317af32d74b46c60d05f0
Alexey <flash-kir@yandex.ru>
2022-09-16 23:38:05 +0300

    add try excrpt

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from urllib.error import HTTPError
 from urllib.parse import urljoin
 
 
-def parse_book_page(book_id):
+def get_book_html(book_id):
     url = f'https://tululu.org/b{book_id}/'
     response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
-    check_for_redirect(response)
-    soup = BeautifulSoup(response.text, 'lxml')
+    '''check_for_redirect(response)'''
+    return response.text
+
+
+def parse_book_page(book_html, book_id):
+    soup = BeautifulSoup(book_html, 'lxml')
     title_block = soup.find('body').find('div', id='content').find('h1')
     title, name = title_block.text.split('::')
     image_path = soup.find('div', class_='bookimage').find('img')['src']
@@ -33,13 +37,20 @@ def check_for_redirect(response):
         raise HTTPError("Не найдена книга")
 
 
-def download_file(url, filename, folder='books/'):
-    response = requests.get(url, allow_redirects=False)
-    response.raise_for_status()
-    check_for_redirect(response)
-    os.makedirs(folder, exist_ok=True)
-    with open(os.path.join(folder, filename), 'wb') as file:
-        file.write(response.content)
+def download_book_text_to_file(book_id, filename, folder='books/'):
+    params = {
+        'id': book_id
+    }
+    try:
+        response = requests.get('https://tululu.org/txt.php', params=params, allow_redirects=False)
+        response.raise_for_status()
+        check_for_redirect(response)
+        os.makedirs(folder, exist_ok=True)
+        with open(os.path.join(folder, filename), 'wb') as file:
+            file.write(response.content)
+    except requests.exceptions.ConnectionError:
+            print('соединение потеряно')
+
 
 
 def download_image(url, folder='images/'):
@@ -59,14 +70,18 @@ def main():
     args = parser.parse_args()
     for book_id in range(args.start_id, args.end_id + 1):
         try:
-            book_data = parse_book_page(book_id)
-            download_file(
-                f'https://tululu.org/txt.php?id={book_id}', 
-                book_data['filename']
-            )
-            download_image(book_data['image_path'])
-        except:
-            HTTPError
+            book_html = get_book_html(book_id)
+            book_content = parse_book_page(book_html, book_id)
+            download_book_text_to_file(book_id, book_content['filename'])
+            download_image(book_content['image_path'])
+        except HTTPError:
+            print('редирект')
+        except AttributeError:
+            print(f'не найдена страница книги {book_id}')
+        except requests.exceptions.HTTPError:
+            print('ссылка не верна')
+        except requests.exceptions.ConnectionError:
+            print('соединение потеряно')
 
 
 if __name__ == '__main__':
