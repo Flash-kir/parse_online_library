f8ae4757e0eda89c2ae295f1c09a8dcd14f86aca
Alexey <flash-kir@yandex.ru>
2022-09-16 22:33:49 +0300

    add urlparse, add comprehensions, del print, resolve name conflict(id), add script description

--- main.py
+++ main.py
@@ -5,36 +5,24 @@ import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filepath
 from urllib.error import HTTPError
-from urllib.parse import urlparse
+from urllib.parse import urljoin
 
 
-def parse_book_page(id):
-    url = f'https://tululu.org/b{id}/'
+def parse_book_page(book_id):
+    url = f'https://tululu.org/b{book_id}/'
     response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
     title_block = soup.find('body').find('div', id='content').find('h1')
     title, name = title_block.text.split('::')
-
     image_path = soup.find('div', class_='bookimage').find('img')['src']
+    comments = [x.find('span').text for x in soup.find('div', id='content').find_all('div', class_='texts')]
+    genres = [x.text for x in soup.find('span', class_='d_book').find_all('a')]
 
-    comments_blocks = soup.find('div', id='content').find_all('div', class_='texts')
-    comments = []
-    for comment_block in comments_blocks:
-        comments.append(comment_block.find('span').text)
-
-    genres_blocks = soup.find('span', class_='d_book').find_all('a')
-    genres = []
-    for genres_block in genres_blocks:
-        genres.append(genres_block.text)
-
-    print('Название: ', sanitize_filepath(f'{title.strip()}'))
-    print('Автор: ', sanitize_filepath(f'{name.strip()}'))
-    print('Жанры: ', ', '.join(genres))
     return {
-        'filename': sanitize_filepath(f'{id}. {title.strip()} - {name.strip()}.txt'),
-        'image_path': f'https://tululu.org/{image_path}',
+        'filename': sanitize_filepath(f'{book_id}. {title.strip()} - {name.strip()}.txt'),
+        'image_path': urljoin('https://tululu.org/', image_path),
         'comments': comments,
         'genres': genres
     }
@@ -65,15 +53,15 @@ def download_image(url, folder='images/'):
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Программа скачивает книги с сайта https://tululu.org/")
     parser.add_argument('start_id', type=int, default=1, help='id книги, с которой начинается скачивание')
     parser.add_argument('end_id', type=int, default=10, help='id книги, которой закончится скачивание')
     args = parser.parse_args()
-    for id in range(args.start_id, args.end_id + 1):
+    for book_id in range(args.start_id, args.end_id + 1):
         try:
-            book_data = parse_book_page(id)
+            book_data = parse_book_page(book_id)
             download_file(
-                f'https://tululu.org/txt.php?id={id}', 
+                f'https://tululu.org/txt.php?id={book_id}', 
                 book_data['filename']
             )
             download_image(book_data['image_path'])
