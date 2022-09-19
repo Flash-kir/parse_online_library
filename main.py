4ba26126f5323d1be2706239981f423e61083f01
Alexey <flash-kir@yandex.ru>
2022-09-19 18:16:32 +0300

    add arguments to god of arguments

--- main.py
+++ main.py
@@ -39,63 +39,68 @@ def check_for_redirect(response):
         raise HTTPError('Старинца не найдена')
 
 
-def download_book_text_to_file(book_id, filename: str, folder='books/'):
+def download_book_text_to_file(book_id, filename: str, dest_folder, folder='books/'):
     params = {
         'id': book_id
     }
     response = requests.get('https://tululu.org/txt.php', params=params, allow_redirects=False)
     response.raise_for_status()
     check_for_redirect(response)
-    os.makedirs(folder, exist_ok=True)
-    with open(os.path.join(folder, filename), 'wb') as file:
+    os.makedirs(os.path.join(dest_folder, folder), exist_ok=True)
+    with open(os.path.join(dest_folder, folder, filename), 'wb') as file:
         file.write(response.content)
-    return os.path.join(folder, filename)
+    return os.path.join(dest_folder, folder, filename)
 
 
 
-def download_image(url, folder='images/'):
+def download_image(url, dest_folder, folder='images/'):
     response = requests.get(url, allow_redirects=True)
     response.raise_for_status()
     check_for_redirect(response)
     filename = url.split('/')[-1]
-    os.makedirs(folder, exist_ok=True)
-    with open(os.path.join(folder, filename), 'wb') as file:
+    os.makedirs(os.path.join(dest_folder, folder), exist_ok=True)
+    with open(os.path.join(dest_folder, folder, filename), 'wb') as file:
         file.write(response.content)
-    return os.path.join(folder, filename)
+    return os.path.join(dest_folder, folder, filename)
 
 
 def get_book_id_from_url(book_url: str):
     return book_url.split('/b')[-1].split('/')[0]
 
 
-def download_book(book_url: str):
+def download_book(book_url: str, dest_folder: str, skip_imgs: bool, skip_txt: bool):
     book_html = get_book_html(book_url)
     book_content = parse_book_page(book_html, book_url)
+    image_src = ''
+    book_path = ''
+    if not skip_imgs:
+        image_src = download_image(book_content['image_path'], dest_folder)
+    if not skip_txt:
+        book_path = download_book_text_to_file(
+            get_book_id_from_url(book_url), 
+            book_content['filename'],
+            dest_folder
+        )
     return {
         'title': book_content['title'],
         'author': book_content['author'],
-        'image_src': download_image(
-            book_content['image_path']
-        ),
-        'book_path': download_book_text_to_file(
-            get_book_id_from_url(book_url), 
-            book_content['filename']
-        ),
+        'image_src': image_src,
+        'book_path': book_path,
         'comments': book_content['comments'],
         'genres': book_content['genres']
     }
 
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Программа скачивает книги с сайта https://tululu.org/")
-    parser.add_argument('start_id', type=int, default=1, help='id книги, с которой начинается скачивание')
-    parser.add_argument('end_id', type=int, default=10, help='id книги, которой закончится скачивание')
-    args = parser.parse_args()
-    for book_id in range(args.start_id, args.end_id + 1):
+def main(start_id: int, end_id: int, dest_folder: str, skip_imgs: bool, skip_txt: bool):
+    for book_id in range(start_id, end_id + 1):
         try:
-            book_url = urljoin('https://tululu.org/', f'b{book_id}/')
-            download_book(book_url)
+            download_book(
+                urljoin('https://tululu.org/', f'b{book_id}/'),
+                dest_folder, 
+                skip_imgs, 
+                skip_txt
+            )
         except HTTPError:
             print('редирект')
         except AttributeError:
@@ -108,4 +113,17 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Программа скачивает книги с сайта https://tululu.org/")
+    parser.add_argument('start_id', type=int, default=1, help='id книги, с которой начинается скачивание')
+    parser.add_argument('end_id', type=int, default=10, help='id книги, которой закончится скачивание')
+    parser.add_argument('-f', '--dest_folder', type=str, default='', help='путь к каталогу с результатами парсинга: картинкам, книгам, JSON')
+    parser.add_argument('-i', '--skip_imgs', type=bool, default=False, help='не скачивать картинки')
+    parser.add_argument('-t', '--skip_txt', type=bool, default=False, help='не скачивать книги')
+    args = parser.parse_args()
+    main(
+        args.start_id, 
+        args.end_id,
+        args.dest_folder,
+        args.skip_imgs,
+        args.skip_txt,
+    )
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -19,7 +19,7 @@ def get_category_page(page_num: int) -> str:
     return response.text
 
 
-def parse_category_page(page_html: str) -> list:
+def parse_category_page(page_html: str, dest_folder: str, skip_imgs: bool, skip_txt: bool) -> list:
     soup = BeautifulSoup(page_html, 'lxml')
     selector = 'body div[id="content"] .bookimage a[href]'
     books_urls = soup.select(selector)
@@ -27,26 +27,47 @@ def parse_category_page(page_html: str) -> list:
     for book_link in books_urls:
         book_url = urljoin(BASE_URL, book_link['href'])
         print(book_url)
-        book_json = download_book(book_url)
+        book_json = download_book(
+            book_url, 
+            dest_folder, 
+            skip_imgs, 
+            skip_txt
+        )
         if book_json:
             if book_json['book_path']:
                 books_jsons.append(book_json)
     return books_jsons
 
 
-def main(start_page: int, end_page: int):
+def main(start_page: int, end_page: int, dest_folder: str, skip_imgs: bool, skip_txt: bool, json_path: str):
     for page_num in range(start_page, end_page):
         page_html = get_category_page(page_num)
-        books = parse_category_page(page_html)
-        with open(f'books({start_page}-{end_page - 1}_pages).json', 'w', encoding='utf-8') as file:
-            json.dump(books, file, ensure_ascii=False)
-
-        
+        books = parse_category_page(page_html, dest_folder, skip_imgs, skip_txt)
+        result_json_path = ''
+        if json_path:
+            result_json_path = json_path
+        elif dest_folder:
+            result_json_path = dest_folder
+        os.makedirs(result_json_path, exist_ok=True)
+        with open(os.path.join(result_json_path, f'books({start_page}-{end_page - 1}_pages).json'), 'w', encoding='utf-8') as file:
+            json.dump(books, file, ensure_ascii=False)        
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Программа скачивает книги с сайта https://tululu.org/")
     parser.add_argument('-s', '--start_page', type=int, default=1, help='номер страницы в разделе фантастики, с которой начинается скачивание')
     parser.add_argument('-e', '--end_page', type=int, default=701, help='номер страницы в разделе фантастики, которой закончится скачивание')
+    parser.add_argument('-f', '--dest_folder', type=str, default='', help='путь к каталогу с результатами парсинга: картинкам, книгам, JSON')
+    parser.add_argument('-i', '--skip_imgs', type=bool, default=False, help='не скачивать картинки')
+    parser.add_argument('-t', '--skip_txt', type=bool, default=False, help='не скачивать книги')
+    parser.add_argument('-j', '--json_path', type=str, default='', help='указать свой путь к *.json файлу с результатами')
     args = parser.parse_args()
-    main(args.start_page, args.end_page)
+    main(
+        args.start_page,
+        args.end_page,
+        args.dest_folder,
+        args.skip_imgs,
+        args.skip_txt,
+        args.json_path
+    )
+
