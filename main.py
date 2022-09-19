35507311f8f54c3c16f298339b3ae90dc7f1f036
Alexey <flash-kir@yandex.ru>
2022-09-19 16:58:19 +0300

    download books from all pages

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ from urllib.error import HTTPError
 from urllib.parse import urljoin
 
 
-def get_book_html(book_id):
-    url = f'https://tululu.org/b{book_id}/'
-    response = requests.get(url, allow_redirects=False)
+def get_book_html(book_url: str):
+    response = requests.get(book_url, allow_redirects=False)
     response.raise_for_status()
     check_for_redirect(response)
     return response.text
 
 
-def parse_book_page(book_html, book_id):
+def parse_book_page(book_html, book_url: str) -> dict:
     soup = BeautifulSoup(book_html, 'lxml')
     title_block = soup.find('body').find('div', id='content').find('h1')
     title, name = title_block.text.split('::')
@@ -26,8 +25,10 @@ def parse_book_page(book_html, book_id):
     genres = [x.text for x in soup.find('span', class_='d_book').find_all('a')]
 
     return {
-        'filename': sanitize_filepath(f'{book_id}. {title.strip()} - {name.strip()}.txt'),
-        'image_path': urljoin(f'https://tululu.org/b{book_id}/', image_path),
+        'title': f'{title.strip()}',
+        'author': f'{name.strip()}',
+        'filename': sanitize_filepath(f'{title.strip()} - {name.strip()}.txt'),
+        'image_path': urljoin(book_url, image_path),
         'comments': comments,
         'genres': genres
     }
@@ -35,10 +36,10 @@ def parse_book_page(book_html, book_id):
 
 def check_for_redirect(response):
     if response.history:
-        raise HTTPError('Не найдена книга')
+        raise HTTPError('Старинца не найдена')
 
 
-def download_book_text_to_file(book_id, filename, folder='books/'):
+def download_book_text_to_file(book_id, filename: str, folder='books/'):
     params = {
         'id': book_id
     }
@@ -48,6 +49,7 @@ def download_book_text_to_file(book_id, filename, folder='books/'):
     os.makedirs(folder, exist_ok=True)
     with open(os.path.join(folder, filename), 'wb') as file:
         file.write(response.content)
+    return os.path.join(folder, filename)
 
 
 
@@ -59,6 +61,30 @@ def download_image(url, folder='images/'):
     os.makedirs(folder, exist_ok=True)
     with open(os.path.join(folder, filename), 'wb') as file:
         file.write(response.content)
+    return os.path.join(folder, filename)
+
+
+def get_book_id_from_url(book_url: str):
+    return book_url.split('/b')[-1].split('/')[0]
+
+
+def download_book(book_url: str):
+    book_html = get_book_html(book_url)
+    book_content = parse_book_page(book_html, book_url)
+    return {
+        'title': book_content['title'],
+        'author': book_content['author'],
+        'image_src': download_image(
+            book_content['image_path']
+        ),
+        'book_path': download_book_text_to_file(
+            get_book_id_from_url(book_url), 
+            book_content['filename']
+        ),
+        'comments': book_content['comments'],
+        'genres': book_content['genres']
+    }
+
 
 
 def main():
@@ -68,10 +94,8 @@ def main():
     args = parser.parse_args()
     for book_id in range(args.start_id, args.end_id + 1):
         try:
-            book_html = get_book_html(book_id)
-            book_content = parse_book_page(book_html, book_id)
-            download_book_text_to_file(book_id, book_content['filename'])
-            download_image(book_content['image_path'])
+            book_url = urljoin('https://tululu.org/', f'b{book_id}/')
+            download_book(book_url)
         except HTTPError:
             print('редирект')
         except AttributeError:
---
+++ parse_tululu_category.py
@@ -0,0 +1,47 @@
+import os
+
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import json
+
+from main import check_for_redirect, download_book
+
+BASE_URL = 'https://tululu.org/'
+FANTASTIC_CATEGORY_URL = urljoin(BASE_URL, 'l55/')
+
+def get_category_page(page_num: int) -> str:
+    url = urljoin(FANTASTIC_CATEGORY_URL, f'{page_num}/')
+    response = requests.get(url, allow_redirects=False)
+    response.raise_for_status()
+    check_for_redirect(response)
+    return response.text
+
+
+def parse_category_page(page_html: str) -> list:
+    soup = BeautifulSoup(page_html, 'lxml')
+    page_books = soup.find('body').find('div', id='content').find_all('div', class_='bookimage')
+    books_jsons = []
+    for page_book in page_books:
+        book_url = urljoin(BASE_URL, page_book.find('a')['href'])
+        print(book_url)
+        book_json = download_book(book_url)
+        if book_json:
+            if book_json['book_path']:
+                books_jsons.append(book_json)
+    return books_jsons
+
+
+def main(max: int):
+    for page_num in range(1, max + 1):
+        page_html = get_category_page(page_num)
+        books = parse_category_page(page_html)
+        with open('books.json', 'w', encoding='utf-8') as file:
+            json.dump(books, file, ensure_ascii=False)
+
+        
+
+
+if __name__ == '__main__':
+    # argparse
+    main(1)
