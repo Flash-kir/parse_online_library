c7f63a3f0decb7ebfc4c01d9ef76c2a311821731
Flash-kir <flash-kir@yandex.ru>
2022-09-26 20:58:37 +0300

    Add links to books txt files.

--- render_website.py
+++ render_website.py
@@ -1,10 +1,8 @@
 import os
 import json
 
-#from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server, shell
 
 
 def fetch_images(folder='./books/images'):
@@ -14,6 +12,7 @@ def fetch_images(folder='./books/images'):
     for filename in os.listdir(images_folder):
         day_images.append(os.path.join(images_folder, f'{filename}'))
     return day_images
+
 
 def main():
     with open(os.path.join('./', 'books.json'), 'r') as books:
@@ -34,13 +33,9 @@ def main():
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
     
-    server = Server()
-    server.watch('*.*', shell('index html'))
-    server.serve(root='.', port='8000')
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
-#    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-#    server.serve_forever()
-#    with open(path, mode='r', encoding='utf-8') as my_file:
 
 if __name__ == '__main__':
     main()
